1d4a9c7d70653d044cc66a4060459c9b04d8ecb1
Isaac Kretzmer <isaack8@uw.edu>
2019-01-16 10:43:21 -0800

    Jupyter Notebook and setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+echo "Creating directory..."
+mkdir HCEPDB
+
+echo "Changing directory..."
+cd HCEPDB
+
+echo "Creating README.txt..."
+touch README.txt
+
+echo "Downloading file..."
+curl -O http://faculty.washington.edu/dacb/HCEPDB_moldata.zip
+
+echo "Uncompressing file..."
+unzip HCEPDB_moldata.zip
+
